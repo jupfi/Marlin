eea19c0399ed1e71587eaf5268c00fca45739891
jupfi <git@jupfi.me>
2019-11-27 23:09:27 +0100

    Funktioniert wieder

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -774,7 +774,7 @@
  */
 #define X_PROBE_OFFSET_FROM_EXTRUDER 23  // X offset: -left  +right  [of the nozzle]
 #define Y_PROBE_OFFSET_FROM_EXTRUDER -53   // Y offset: -front +behind [the nozzle]
-#define Z_PROBE_OFFSET_FROM_EXTRUDER  0//-1.8   // Z offset: -below +above  [the nozzle]
+#define Z_PROBE_OFFSET_FROM_EXTRUDER  0.4   // Z offset: -below +above  [the nozzle]
 
 // Certain types of probes need to stay away from edges
 #define MIN_PROBE_EDGE 10
