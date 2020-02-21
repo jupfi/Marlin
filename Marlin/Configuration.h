364cf726237bcf0bfa2b56c9f51347367826089c
jupfi <git@jupfi.me>
2020-02-21 14:57:33 +0100

    Update Z Probe Position & steps per unit z

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -607,7 +607,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 1600, 98.04}
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 98.04}
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -774,7 +774,7 @@
  */
 #define X_PROBE_OFFSET_FROM_EXTRUDER 23  // X offset: -left  +right  [of the nozzle]
 #define Y_PROBE_OFFSET_FROM_EXTRUDER -53   // Y offset: -front +behind [the nozzle]
-#define Z_PROBE_OFFSET_FROM_EXTRUDER  0.4   // Z offset: -below +above  [the nozzle]
+#define Z_PROBE_OFFSET_FROM_EXTRUDER  0.0   // Z offset: -below +above  [the nozzle]
 
 // Certain types of probes need to stay away from edges
 #define MIN_PROBE_EDGE 10
